7d1b4ed7911f21419302e9f1ee96b220a2cd83d7
Hecater88 <47868066+Hecater88@users.noreply.github.com>
2023-05-30 18:30:10 +0200

    About done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => {
+	return (
+		<Tilt className="xs:w-[250px] w-full">
+			<motion.div
+				variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+				className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
+				<div
+					options={{ max: 45, scale: 1, speed: 450 }}
+					className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+					<img src={icon} alt={title} className="w-16 h-16 object-contain" />
+					<h3 className="text-white text-[20px] font-bold text-center">
+						{title}
+					</h3>
+				</div>
+			</motion.div>
+		</Tilt>
+	);
+};
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+	return (
+		<>
+			<motion.div variants={textVariant()}>
+				<p className={styles.sectionSubText}>Introduction</p>
+				<h2 className={styles.sectionHeadText}>Overview</h2>
+			</motion.div>
 
-export default About
+			<motion.p
+				variants={fadeIn("", "", 0.1, 1)}
+				className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+				Me considero un programador Frontend y desarrollador de videojuegos con
+				experiencia en el uso de JavaScript y TypeScript, así como un amplio
+				conocimiento en React. También tengo experiencia con Node.js, lo que me
+				ha permitido crear aplicaciones completas y escalables en el entorno del
+				servidor. A lo largo de mi trayectoria profesional, he adquirido una
+				valiosa experiencia en la ingeniería frontend y la programación de
+				videojuegos. He trabajado en empresas indies de videojuegos y startups,
+				donde he demostrado habilidades técnicas sólidas y creatividad en el
+				desarrollo de interfaces de usuario y experiencias de juego.
+			</motion.p>
+
+			<div className="mt-20 flex flex-wrap gap-10">
+				{services.map((service, index) => (
+					<ServiceCard key={service.title} index={index} {...service} />
+				))}
+			</div>
+		</>
+	);
+};
+
+export default SectionWrapper(About, "about");
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,19 +4,52 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
 	const computer = useGLTF("./desktop_pc/scene.gltf");
 
 	return (
 		<mesh>
 			<hemisphereLight intensity={0.15} groundColor="black" />
 			<pointLight intensity={1} />
-			<primitive object={computer.scene} />
+			<spotLight
+				position={[-20, 50, 10]}
+				angle={0.12}
+				penumbra={1}
+				intensity={1}
+				castShadow
+				shadow-mapSize={1024}
+			/>
+			<primitive
+				object={computer.scene}
+				scale={isMobile ? 0.7 : 0.75}
+				position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+				rotation={[-0.01, -0.2, -0.1]}
+			/>
 		</mesh>
 	);
 };
 
 const ComputerCanvas = () => {
+	const [isMobile, setIsMobile] = useState(false);
+
+	useEffect(() => {
+		//listener for changes to the screen size
+		const mediaQuery = window.matchMedia("(max-width:500px)");
+		//set the initial value
+		setIsMobile(mediaQuery.matches);
+		//define callback function to handle change to the media query
+		const handleMediaQueryChange = (event) => {
+			setIsMobile(event.matches);
+		};
+		//add the callback as a listener for chanfe
+		mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+		//remove the listener when component is unmounted
+		return () => {
+			mediaQuery.removeEventListener("change", handleMediaQueryChange);
+		};
+	}, []);
+
 	return (
 		<Canvas
 			frameloop="demand"
@@ -29,7 +62,7 @@ const ComputerCanvas = () => {
 					maxPolarAngle={Math.PI / 2}
 					minPolarAngle={Math.PI / 2}
 				/>
-				<Computers />
+				<Computers isMobile={isMobile} />
 			</Suspense>
 			<Preload all />
 		</Canvas>
