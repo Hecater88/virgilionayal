bd59fffbd753e995c3e3dc1155c2f262b232ccb0
Hecater88 <47868066+Hecater88@users.noreply.github.com>
2023-05-31 20:09:15 +0200

    Contact section css done

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -3,6 +3,7 @@ import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 import { testimonials } from "../constants";
+
 const FeedbackCard = ({
 	index,
 	testimonial,
@@ -10,7 +11,7 @@ const FeedbackCard = ({
 	designation,
 	company,
 	image,
-}) => {
+}) => (
 	<motion.div
 		variants={fadeIn("", "spring", index * 0.5, 0.75)}
 		className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full">
@@ -34,8 +35,9 @@ const FeedbackCard = ({
 				/>
 			</div>
 		</div>
-	</motion.div>;
-};
+	</motion.div>
+);
+
 const Feedbacks = () => {
 	return (
 		<div className="mt-12 bg-black-100 rounded-[20px]">
@@ -47,7 +49,7 @@ const Feedbacks = () => {
 				</motion.div>
 			</div>
 
-			<div className={`${styles.paddingX} mt-20 pb-14 flex flex-wrap gap-7`}>
+			<div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
 				{testimonials.map((testimonial, index) => (
 					<FeedbackCard key={testimonial.name} index={index} {...testimonial} />
 				))}
