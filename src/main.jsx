d9c91b394f421cd1df3e8dc55d263cba257cf981
Hecater88 <47868066+Hecater88@users.noreply.github.com>
2023-06-08 17:50:05 +0200

    fix Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { inject } from "@vercel/analytics";
-
-inject();
+import { Analytics } from "@vercel/analytics/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<App />
+		<Analytics />
 	</React.StrictMode>
 );
